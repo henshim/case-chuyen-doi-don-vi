3b8de5db5e4127121a9777c0f17c90b0c4cb1cd0
henshim <hieume113@gmail.com>
2020-12-02 11:24:41 +0700

    case study

---
+++ code.js
@@ -0,0 +1,26 @@
+function doi() {
+    let inputValue = +document.getElementById('inputValue').value;
+    let from = +document.getElementById('from').value;
+    let to = +document.getElementById('to').value;
+    var result = 0;
+    result = to * (inputValue / from);
+    document.getElementById('output').value = result;
+}
+
+function decimalAdjust(){
+    let a=Math.ceil(document.getElementById('output').value)
+    document.getElementById('output').value=a;
+}
+
+function keyPress(event){
+    if (event.keyCode==13){
+        let inputValue = +document.getElementById('inputValue').value;
+        let from = +document.getElementById('from').value;
+        let to = +document.getElementById('to').value;
+        var result = 0;
+        result = to * (inputValue / from);
+        document.getElementById('output').value = result;
+    }
+}
+
+addEventListener('keypress',keyPress);
